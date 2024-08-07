ff19d5caa376568ecf47bdf93924d0c744397438
Henry Strickland <strick@yak.net>
2024-08-07 00:52:47 -0700

    work on os9level1v2.py

--- firmware/os9level1v2.py
+++ firmware/os9level1v2.py
@@ -8,82 +8,50 @@ OUT_HIGH, OUT_LOW, IN_HIGH = rp2.PIO.OUT_HIGH, rp2.PIO.OUT_LOW, rp2.PIO.IN_HIGH
 
 @rp2.asm_pio(
     in_shiftdir=rp2.PIO.SHIFT_LEFT,   # For bits into shift register
-    autopush=True,                    # Automatically push the next word from input shift reg into FIFO
+    autopush=True,
     push_thresh=16,                    # Push to the FIFO when 16 bits of the ISR have been produced
+    autopull=False,
     out_shiftdir=rp2.PIO.SHIFT_RIGHT,
-    sideset_init=[OUT_HIGH] * 5,
+    out_init=tuple(16 * [IN_HIGH]),
+    sideset_init=tuple(5 * [OUT_HIGH]), #  EBar, QBar, M0, M1, M2
 )
 def pio_cycle():
     EBAR  = 0b_000_01
     QBAR  = 0b_000_10
+    MODE0 = 0b_000_00
     MODE1 = 0b_001_00
     MODE2 = 0b_010_00
     MODE3 = 0b_011_00
     MODE4 = 0b_100_00
+    MODE5 = 0b_101_00
+    MODE6 = 0b_110_00
+    MODE7 = 0b_111_00
 
     wrap_target()
     label("top")
-
-    set(y, 31)           .side(MODE1 | EBAR | QBAR)
-    label("delay1")
-    jmp(y_dec, "delay1")
-
-    # Receive Address.
-    in_(pins, 16)
-    # push()
-
-    set(y, 31)           .side(MODE2 | EBAR) # Raise Q and use mode 2
-    label("delay2")
-    jmp(y_dec, "delay2")
-
-    # Receive Extra signals (with too-early data bus).
-    in_(pins, 16)
-    # push()
-
+    set(x, 23)        .side(MODE0 | EBAR | QBAR)
+    in_(x, 16)        .side(MODE1 | EBAR | QBAR)     # rx 23
+    in_(pins, 16)     .side(MODE1 | EBAR | QBAR)     # rx addr
+    nop()             .side(MODE2 | EBAR)
+    in_(pins, 16)     .side(MODE2 | EBAR)            # rx flags
+    nop()             .side(MODE2 )
     # Jump if reading
-    jmp(8, "cpu_reads")  # GPIO 8 is R/W, when in MODE2.
+    jmp(pin, "read")    .side(MODE2 )                # GPIO 8 is R/W, when in MODE2.
 
-    ########################################################
-    # CPU WRITING (pico Rx)
+    # Cpu writes, pico RX
+    nop()             .side(MODE2 |        QBAR)
+    in_(pins, 16)     .side(MODE2 |        QBAR)     # rx data
+    jmp("top")        .side(MODE2 | EBAR | QBAR)
 
-    set(y, 31)           .side(MODE2) # Raise E and stay in mode 2
-    label("delay3")
-    jmp(y_dec, "delay3")
-
-    set(y, 31)           .side(MODE2 | QBAR) # Drop Q and stay in mode 2
-    label("delay4")
-    jmp(y_dec, "delay4")
-
-    # finally read the data bus.
-    in_(pins, 16)
-    jmp("top")
-
-    ########################################################
-    # CPU READING  ( pico Tx )
-
-    label("cpu_reads")
-    set(y, 31)
-    pull()               .side(MODE3) # Raise E and use mode 3
-    out(pindirs, 16)
-    pull()
-    out(pins, 16)
-    pull()
-    out(pindirs, 16)
-
-    set(y, 31)           .side(MODE3) # Raise E and use mode 3
-    label("delay5")
-    jmp(y_dec, "delay5")
-
-    set(y, 31)           .side(MODE3 | QBAR) # Drop Q and use mode 3
-    label("delay6")
-    jmp(y_dec, "delay6")
-
-    set(y, 15)           .side(MODE3 | QBAR | QBAR) # Drop E and use mode 3
-    label("delay7")          # shorter delay, for cpu to latch Data bus.
-    jmp(y_dec, "delay7")
-
+    # Cpu reads, pico TX
+    label("read")
+    pull(block)       .side(MODE2 |        QBAR )
+    out(pindirs, 16)  .side(MODE2 |        QBAR )
+    pull(block)       .side(MODE3 |        QBAR )
+    out(pins, 16)     .side(MODE3 |        QBAR )
+    pull(block)       .side(MODE3 | EBAR | QBAR)
+    out(pindirs, 16)  .side(MODE2 | EBAR | QBAR)
     wrap()
-    ########################################################
 
 
 CYCLE_PIO = 0
@@ -107,16 +75,23 @@ class CycleMachine:
         self.sm_cycle = rp2.StateMachine(
             (CYCLE_PIO << 3) + CYCLE_SM, # which state machine in pio0
             pio_cycle,
-            freq=125_000_000,
-            # freq=125_000_000 >> 4,
+            # freq=125_000_000,
+            freq=125_000_000 >> 6,
             in_base=0,
             out_base=0,
             sideset_base=16,
+            set_base=16,
+            jmp_pin=8,
         )
         print('done set up')
 
     def start_state_machines(self):
-        # self.sm_cycle.restart()
+        self.sm_cycle.restart()
+        self.sm_cycle.restart()
+        self.sm_cycle.restart()
+        self.sm_cycle.restart()
+        self.sm_cycle.restart()
+
         self.sm_cycle.active(True)
         print('done started')
 
@@ -220,50 +195,40 @@ ResetTheCPU()
 
 with CycleMachine() as cm:
     cm.start_state_machines()
-
-    start = 0
     vma = 0
+    start = 0
+    trigger = 0
+    while not cm.sm_cycle.rx_fifo():
+        pass  # Wait for an address.
     while True:
-        print("loop", end=" ")
-        while not cm.sm_cycle.rx_fifo():
-            pass  # Wait for an address.
-        addr = cm.sm_cycle.get()
-        print("addr %x" % addr, end=" ")
+        #@# while not cm.sm_cycle.rx_fifo(): pass  # Wait for 23.
+        twenty_three = cm.sm_cycle.get()
+        if twenty_three != 23:
+            raise Exception("wanted 23 got %d." % twenty_three)
 
-        while not cm.sm_cycle.rx_fifo():
-            pass  # Wait for a flags value.
+        #@# while not cm.sm_cycle.rx_fifo(): pass  # Wait for an address.
+        addr = cm.sm_cycle.get()
+
+        #@# while not cm.sm_cycle.rx_fifo(): pass  # Wait for flags.
         flags = cm.sm_cycle.get()
-        reading = (flags & 0x100)
-        print("flags %x" % addr, end=" ")
 
         if 0xFF00 <= addr < 0xFFF0:
-            print("*** IO PAGE *** addr=%x flags=%x reading=%x" % (addr, flags, reading))
-            break
+            trigger = 1
 
-        if vma:
-            if reading:
-                x = Ram[addr]
-                cm.sm_cycle.put(0x0000)
-                cm.sm_cycle.put(x)
-                cm.sm_cycle.put(0xFFFF)
-                if start:
-                    print("@ %04x start %02x" % (addr, x))
-                else:
-                    print("@ %04x read %02x" % (addr, x))
-            else:
-                print("writing", end=" ")
-                while not cm.sm_cycle.rx_fifo():
-                    pass  # Wait for a flags value.
-                x = 255 & cm.sm_cycle.get()
-                Ram[addr] = x
-                print("@ %04x write %02x" % (addr, x))
-            print(';')
+        reading = (flags & 0x100)
+        if reading:
+            data = Ram[addr] if vma else 0xFF
+            cm.sm_cycle.put(0x00FF)  # Pindirs: outputs
+            cm.sm_cycle.put(data)       # Pins
+            cm.sm_cycle.put(0x0000)  # Pindirs: inputs
+            if trigger and vma: print("%s %x %x" % ("@" if start else "r", addr, data))
+            # if vma: print("%s %x /%x %x" % ("@" if start else "r", addr, flags>>8, data))
         else:
-            cm.sm_cycle.put(0x0000)
-            cm.sm_cycle.put(0xA5)
-            cm.sm_cycle.put(0xFFFF)
-            print('/')
-
+            #@# while not cm.sm_cycle.rx_fifo(): pass  # Wait for data.
+            data = 255 & cm.sm_cycle.get()
+            Ram[addr] = data
+            if trigger: print("w %x %x" % (addr, data))
+            # print("w %x /%x %x" % (addr, flags>>8, data))
         vma = flags & 0x200   # AVMA bit means next cycle is Valid
         start = flags & 0x400 # LIC bit means next cycle is Start
 
