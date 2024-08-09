67c81578a71497a02643f735506893de54e79438
Henry Strickland <strick@yak.net>
2024-08-09 00:03:08 -0700

    H.6.boot.rom.h

---
+++ firmware/c/H.6.boot.rom.h
@@ -0,0 +1,22 @@
+ 2220  ]a H.4nos H.5.borges.join 
+ 2221  ]c -a 
+ 2222  vi tfr901main.cpp 
+ 2223  ci-l tfr901main.cpp 
+ 2224  vi tfr901main.cpp 
+ 2225  ll
+ 2226  py generate-bootrom-header.py < /home/strick/NEW/nitros9/level1/tfr9/bootroms/nos96809l1.rom > boot.rom.h 
+ 2227  touch tfr901main.cpp 
+ 2228  py generate-bootrom-header.py < /home/strick/NEW/nitros9/level1/tfr9/bootroms/nos96809l1.rom > boot.rom.h 
+ 2229  touch tfr901main.cpp 
+ 2230  py generate-bootrom-header.py < /home/strick/NEW/nitros9/level1/tfr9/bootroms/nos96809l1.rom > boot.rom.h 
+ 2231  touch tfr901main.cpp 
+ 2232  py generate-bootrom-header.py < /home/strick/NEW/nitros9/level1/tfr9/bootroms/nos96809l1.rom > boot.rom.h 
+ 2233  touch tfr901main.cpp 
+ 2234  vi tfr901main.cpp 
+ 2235  py generate-bootrom-header.py < /home/strick/NEW/nitros9/level1/tfr9/bootroms/nos96809l1.rom > boot.rom.h 
+ 2236  touch tfr901main.cpp 
+ 2237  vi tfr901main.cpp 
+ 2238  ci-l tfr901main.cpp
+ 2239  vi tfr901main.cpp 
+ 2240  history 22
+ 2241  history 22 > H.6.boot.rom.h
