20fd62d4343892f5186c766c7b0f84e858264ee0
Henry Strickland <strick@yak.net>
2025-06-12 01:10:09 -0700

    launchers for /v3/

--- v3/launchers/launch-2500-to-2602.asm
+++ v3/launchers/launch-2500-to-2602.asm
@@ -1,3 +1,8 @@
+; launch-2500-to-2602.asm
+; Produce 256 bytes, [ $2500 : $2600 ).
+; Entry at $2500.
+; Disable interrupts, start a stack, and jump to $2602.
+
     ORG $2500
 
     orcc #$50           ; disable interrupts
