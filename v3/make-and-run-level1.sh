ccaa65cc56f6f4f32c197b93828043cc777dfd52
Henry Strickland <strick@yak.net>
2025-06-12 01:47:06 -0700

    delete old make-and-run-level1.sh

--- v3/make-and-run-level1.sh
+++
@@ -1,9 +0,0 @@
-##!/bin/bash
-#
-#cd $(dirname $0) || exit 1
-#
-#cp -vf with-level1.ports tfr9ports.py
-#
-#cp -vf with-level1.create create-dd.sh
-#
-#- make    && cp -v  build-tmanager903/tmanager.uf2 /media/strick/RPI-RP2/ && ( cd tconsole ; time go run --tags=level1,coco1 tconsole.go  2>_2 | tee _1 )
