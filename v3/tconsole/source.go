b687d7b14a0633eded67357f06e7e867af4a9794
Henry Strickland <strick@yak.net>
2025-10-28 20:04:40 -0700

    AsmSourceLine handles empty module name

--- v3/tconsole/source.go
+++ v3/tconsole/source.go
@@ -3,6 +3,12 @@ package main
 var Sources = make(map[string]*ModSrc)
 
 func AsmSourceLine(modName string, offset uint) string {
+	if modName == "" && LinkSrc != nil {
+		if rec, ok := LinkSrc.Src[offset]; ok {
+			return rec
+		}
+	}
+
 	if modName == "^^" && LinkSrc != nil {
 		if rec, ok := LinkSrc.Src[offset]; ok {
 			return rec
