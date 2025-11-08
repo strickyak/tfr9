b6ec477a68deb806d7633fabce08b93ca0053b6c
Henry Strickland <strick@yak.net>
2025-11-08 10:19:04 -0800

    more demos Makefiles

--- v3/demos-metal-gcc/app-t9sim.h
+++ v3/demos-metal-gcc/app-t9sim.h
@@ -1,8 +1,8 @@
 #ifndef _TFR9_V3_APPS_APP_T9SIM_H_
 #define _TFR9_V3_APPS_APP_T9SIM_H_
 
-#include "v3/apps-metal-gcc/t9sim.h"
-#include "v3/apps-metal-gcc/types.h"
+#include "v3/demos-metal-gcc/t9sim.h"
+#include "v3/demos-metal-gcc/types.h"
 
 void abort(void);
 int getchar();
