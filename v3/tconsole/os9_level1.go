e621aa09b421417ebf52fb8a2083cd9e70cffc8c
Henry Strickland <strick@yak.net>
2026-03-20 17:57:15 -0700

    oops, Os9Level1.MemoryModuleOf was being used by centipede

--- v3/tconsole/os9_level1.go
+++ v3/tconsole/os9_level1.go
@@ -93,9 +93,9 @@ func ScanRamForMemoryModules(ram []byte) []*ScannedModuleInfo {
 var InitialMemoryModules []*ScannedModuleInfo
 
 func (o *Os9Level1) MemoryModuleOf(addr uint) (name string, offset uint) {
-	if *CENTIPEDE {
-		return "", addr
-	}
+	// if *CENTIPEDE {
+		// return "", addr
+	// }
 	if *NO_MODULES {
 		return "", addr
 	}
