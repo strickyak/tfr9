f081f29ce09710d73ca811884b502f39c11e139b
Henry Strickland <strick@yak.net>
2026-08-21 01:05:47 -0400

    v3/tether.sh documents how to run our new tether

---
+++ v3/tether.sh
@@ -0,0 +1,44 @@
+
+go run github.com/strickyak/tfr9-merging-centipede/v1/tether -centipede -level 1 -borges /tmp/veryturbos-chelsea-2026-08-18/borges/  2>_log  | tee _out
+exit
+
+COMMENT << END
+strick@xor:~/modoc/coco-shelf/tfr9/v3/merging/centipede$ go run github.com/strickyak/tfr9-merging-centipede/v1/tether -centipede -level 1 -borges /tmp/veryturbos-chelsea-2026-08-18/borges/  2>_log  | tee _out
+Internal web server started at ":8080"
+:0:
+:1:
+:2:
+[p=1]
+[b=tfr9]
+[v=911h]
+[mhz=250]
+[h=pizga.net]
+[id=uks]
+[seed=6416156c-5f7a-11f1-bab5-c7f324d6838f]
+:g:
+IDoTurbo9os Install_OS...
+DoTurbo9os Install_OS... n=844b begin=7ab5
+DoTurbo9os Install_OS... begin=7ab5 entry=7ac9
+
+Shell
+
+TOS:mdir
+ Module directory at 00:00:59
+kernel
+init
+tk
+ioman
+scf
+scvt
+term
+go
+shell
+mfree
+mdir
+procs
+sleep
+Basic09
+
+
+TOS:
+END
