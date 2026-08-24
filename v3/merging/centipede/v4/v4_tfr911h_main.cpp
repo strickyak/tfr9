97875d1f5b2d5eb964dccc5b13f2255b39d4d631
Henry Strickland <strick@yak.net>
2026-08-24 18:21:04 -0400

    define CLOCK_IRQ 1; turn on Clock IRQ again. works.

--- v3/merging/centipede/v4/v4_tfr911h_main.cpp
+++ v3/merging/centipede/v4/v4_tfr911h_main.cpp
@@ -12,7 +12,7 @@
 #define DEBUG_TCL_REPL 0
 // TransmitWrite now routes through fg2bg FIFO — safe from core 1.
 #define HALT_TEST 0
-#define CLOCK_IRQ 0             // 0 to disable 60Hz timer IRQ during debugging
+#define CLOCK_IRQ 1             // 0 to disable 60Hz timer IRQ during debugging
 #define DUMP_FIRST_CYCLES 16    // Log the first N bus cycles after boot for debugging
 #define MHz 250  // clock speed
 
